e89a00653f16d8dc015a92d7917d6e685fb17f6f
Maxim <maks.bidenko.96@mail.ru>
2022-11-28 11:02:53 +0300

    Fixed

--- ex02.java
+++ ex02.java
@@ -1,17 +1,20 @@
 package hw01;
+
 public class ex02 {
     public static void main(String[] args) {
 
-    boolean b = true;
-    for (int P = 2; P <= 1000; P++) {
-        for (int i = 2; i < P; i++) {
-            if (P % i == 0) {
-                b = false;
-                break;
+        boolean b = true;
+        for (int P = 2; P <= 1000; P++) {
+            for (int i = 2; i < P; i++) {
+                if (P % i == 0) {
+                    b = false;
+                    break;
+                }
             }
+            if (b)
+                System.out.println(P);
+            else
+                b = true;
         }
-        if (b) System.out.println(P);
-        else b = true;
     }
-} 
 }
--- ex03.java
+++ ex03.java
@@ -2,31 +2,34 @@ package hw01;
 
 public class ex03 {
     public static void main(String[] args) {
-        System.out.println(sumCalcul(3,2));
-        System.out.println(subtractionCalcul(1,2));
-        System.out.println(multiCalcul(5,2));
-        System.out.println(divideCalcul(5,2));
-       
-}
-    public static double sumCalcul(double a, double b) {
-        double sum = a+b;
-        return sum;
-        
-    }
-    public static double subtractionCalcul(double a, double b) {
-        double subtraction = a-b;
-        return subtraction;
-        
-    }
-    public static double multiCalcul(double a, double b) {
-        double subtraction = a*b;
-        return subtraction;
-        
-    }
-    public static double divideCalcul(double a, double b) {
-        double subtraction = a/b;
-        return subtraction;
-        
-    }
-}
+        System.out.println(sumCalcul(3, 2));
+        System.out.println(subtractionCalcul(1, 2));
+        System.out.println(multiCalcul(5, 2));
+        System.out.println(divideCalcul(5, 2));
 
+    }
+
+    public static double sumCalcul(double a, double b) {
+        double sum = a + b;
+        return sum;
+
+    }
+
+    public static double subtractionCalcul(double a, double b) {
+        double subtraction = a - b;
+        return subtraction;
+
+    }
+
+    public static double multiCalcul(double a, double b) {
+        double subtraction = a * b;
+        return subtraction;
+
+    }
+
+    public static double divideCalcul(double a, double b) {
+        double subtraction = a / b;
+        return subtraction;
+
+    }
+}
