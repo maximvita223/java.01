bfb7e4fcda70490fa44d9ff1fb22e69b1e997411
Maxim <maks.bidenko.96@mail.ru>
2022-11-28 10:52:49 +0300

    Сделал 2 задачу

---
+++ ex02.java
@@ -0,0 +1,17 @@
+package hw01;
+public class ex02 {
+    public static void main(String[] args) {
+
+    boolean b = true;
+    for (int P = 2; P <= 1000; P++) {
+        for (int i = 2; i < P; i++) {
+            if (P % i == 0) {
+                b = false;
+                break;
+            }
+        }
+        if (b) System.out.println(P);
+        else b = true;
+    }
+} 
+}
